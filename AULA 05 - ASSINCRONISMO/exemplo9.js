b9efe04bdb8b3a3296b8781280250e5ac4361e53
emotton <emotton@gmail.com>
2021-09-09 18:41:47 -0300

    Alteração no exemplo9 - Promise.allSettled

---
+++ AULA 05 - ASSINCRONISMO/exemplo9.js
@@ -0,0 +1,36 @@
+function espera_ai5(n){
+    return new Promise((resolve, reject) => {
+        setTimeout(function () {
+            console.log(`[${n}] obrigado por aguardar (5) ...`)
+            resolve("ok5")
+        }, 5000);
+    })
+}
+function espera_ai10(n){
+    return new Promise((resolve, reject) => {
+        setTimeout(function () {
+            console.log(`[${n}] obrigado por aguardar (10) ...`)
+            resolve("ok10")
+        }, 10000);
+    })
+}
+
+function espera_ai20(n){
+    return new Promise((resolve, reject) => {
+        setTimeout(function () {
+            console.log(`[${n}] obrigado por aguardar (20) ...`)
+            reject("zuou tudo")
+        }, 10000);
+    })
+}
+
+const ret1 = espera_ai10(1)
+const ret2 = espera_ai5(2)
+const ret3 = espera_ai10(3)
+const ret4 = espera_ai20(4)
+
+Promise.allSettled([ret1, ret2, ret3, ret4]).then((valores)=>{
+    console.log(valores)
+})
+
+
