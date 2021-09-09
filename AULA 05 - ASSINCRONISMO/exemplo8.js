628d17a8fefb25bf14d927849e7bae4c957fd5b7
emotton <emotton@gmail.com>
2021-09-09 18:33:03 -0300

    Alteração no exemplo8 - [n]

--- AULA 05 - ASSINCRONISMO/exemplo8.js
+++ AULA 05 - ASSINCRONISMO/exemplo8.js
@@ -1,23 +1,23 @@
-function espera_ai5(){
+function espera_ai5(n){
     return new Promise((resolve, reject) => {
         setTimeout(function () {
-            console.log('obrigado por aguardar (5) ...')
+            console.log(`[${n}] obrigado por aguardar (5) ...`)
             resolve("ok5")
         }, 5000);
     })
 }
-function espera_ai10(){
+function espera_ai10(n){
     return new Promise((resolve, reject) => {
         setTimeout(function () {
-            console.log('obrigado por aguardar (10)...')
+            console.log(`[${n}] obrigado por aguardar (10) ...`)
             resolve("ok10")
         }, 10000);
     })
 }
 
-const ret1 = espera_ai5()
-const ret2 = espera_ai10()
-const ret3 = espera_ai10()
+const ret1 = espera_ai10(1)
+const ret2 = espera_ai5(2)
+const ret3 = espera_ai10(3)
 
 Promise.all([ret1, ret2, ret3]).then((valores)=>{
     console.log(valores)
