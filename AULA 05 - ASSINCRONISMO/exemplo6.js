a3b7c5895e128ba253126adff5c1d64acb4fd0ce
emotton <emotton@gmail.com>
2021-09-10 15:48:21 -0300

    Atualização interessante de destructuring

--- AULA 05 - ASSINCRONISMO/exemplo6.js
+++ AULA 05 - ASSINCRONISMO/exemplo6.js
@@ -11,12 +11,12 @@ const lerArquivo = file => new Promise((resolve, reject) =>{
 
 async function init(){
     try {
-        const dados1 = await lerArquivo('./arquivo2.txt')
-        const dados2 = await lerArquivo('./arquivo1.txt')
+        const {data:dados1} = await lerArquivo('./arquivo2.txt')
+        const {data:dados2} = await lerArquivo('./arquivo1.txt')
         console.log('\n************ ARQUIVO[1] ***********\n')
-        console.log(dados1.data);
+        console.log(dados1);
         console.log('\n************ ARQUIVO[2] ***********\n')
-        console.log(dados2.data);
+        console.log(dados2);
         console.log('************** Ao final exibe essa linha ******************')
     } catch(err){
         console.log("Deu erro : ", err)
